c9ccb875b4cd59891c47d76fafe619da5368ebfb
Nasa <nasa.freaks@gmail.com>
2013-07-02 23:05:03 +0700

    Update templates and forms

--- app/views.py
+++ app/views.py
@@ -1,8 +1,14 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
+from forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     #return "Hello, world"
     return render_template('home.html')
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', title = 'Sign in', form = form)
